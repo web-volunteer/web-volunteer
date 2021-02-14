cc5801271ddc06437be3366fa9938ca35e4b31c8
Aylin Ünal <ayl.uenal@gmail.com>
2021-02-14 18:22:46 +0100

    add Owner Schema and profile form

--- models/Owner.js
+++ models/Owner.js
@@ -5,7 +5,25 @@ const ownerSchema = new Schema({
     type: String,
     unique: true
   },
-  password: String
+  password: String,
+  email: String,
+  nameOrganisation: String,
+  firstName: String,
+  lastName: String,
+  location: String,
+  languages: [{
+    type: String
+  }],
+  category: {
+    type: String,
+    enum: ['NGO', 'Individual', 'Association', 'Educational institution', 'Startup', 'Company', 'Social Services', 'other']
+  },
+  description: String,
+  projects: {
+    type: Schema.Types.ObjectId,
+    ref: 'Project'
+  }
+
 });
 
 const Owner = model("Owner", ownerSchema);
